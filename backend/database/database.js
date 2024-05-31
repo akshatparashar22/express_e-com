f315b06255279a5a9e5b7efd6b65a864da47500f
akshatparashar22 <akshatparashar2001@gmail.com>
2024-05-31 23:50:40 +0530

    Connected server to mongoDB

---
+++ backend/database/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const connectDatabase = () => {
+    console.log("db uri found!!",process.env.DB_URI);
+    mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then((data) => {
+        console.log(`MongoDB connected with Server ${data.connection.host}`);
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
+};
+
+module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require("./app")
-const express = require("express");
 const dotenv = require("dotenv");
 
+// config path
 dotenv.config({path:'backend/config/config.env'})
+
+const connectDatabase = require("./database/database")
+//Connecting to DB
+connectDatabase();
 
 app.listen(process.env.PORT,() => {
     console.log(`Server running on localhost http://localhost:${process.env.PORT}`);
