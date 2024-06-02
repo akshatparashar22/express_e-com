f582acd9f5e25eb2db02eb0618afbcc9842a3526
akshatparashar22 <akshatparashar2001@gmail.com>
2024-06-02 14:11:45 +0530

    Created Product CRUD APIs

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,76 @@
-exports.getAllProducts = (req,res) => {
-    res.status(200).json({
-        message: "Route is working fine"
+const Product = require("../models/productModels")
+
+//create Product --Admin
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
     })
+}
+
+//Update Product --Admin
+exports.updateProduct = async (req, res, nex) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not Found!"
+        }); 
+    } else {
+        product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        });
+        res.status(200).json({
+            success: true,
+            product
+        })
+    }
+}
+
+//Delete Product --Admin
+exports.deleteProduct = async (req, res, nex) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not Found!"
+        }); 
+    } else {
+        await product.remove();
+        res.status(200).json({
+            success: true,
+            message: "Product deleted successfully"
+        })
+    }
+}
+
+exports.getAllProducts = async (req,res) => {
+
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        products
+    })
+}
+
+exports.getProduct = async (req,res,next) => {
+
+    const product = await product.findById();
+
+    if(!product){
+        res.status(500).json({
+            success: false,
+            message : "Product not found"
+        })
+    } else {
+        res.status(200).json({
+            success: true,
+            product
+        })
+    }
 }
